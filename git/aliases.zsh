25a205a8cb994ecc8cb092b7364de9b45afbf826
Adam Fraser <adamfraser0@gmail.com>
2017-10-26 13:14:56 -0400

    Add more git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,13 +7,13 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+alias gpp='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
-
+alias gl='glg $(git show-ref | cut -d " " -f 2 | grep -v stash$) HEAD'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
@@ -22,3 +22,19 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+alias gd='git diff'
+alias gap='git add -p'
+
+
+function git-log-graph() {
+	git log --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %s %C(green bold)- %an %C(black bold)%cd (%cr)%Creset' --abbrev-commit --date=short "$@"
+}
+
+# git log
+function glg() {
+if [[ $# == 0 ]] && git rev-parse @{u} &> /dev/null; then
+	git-log-graph @{u} HEAD
+     else
+	git-log-graph "$@"
+     fi
+}
